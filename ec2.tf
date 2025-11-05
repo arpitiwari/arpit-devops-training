f2acf543dae74732d34e167dffa236a2e9955ffe
Arpit Tiwari <arpi2art@gmail.com>
2025-11-05 07:26:33 +0000

    implementing variables

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "arpit-devops" {
 
-    ami = "ami-0a25a306450a2cba3"
-    instance_type = "t2.nano"
-    key_name = "roche-key"
+    #ami = "ami-0a25a306450a2cba3"
+    ami = var.arpit-ami-id
+    instance_type = var.vm-size
+    key_name = var.ec2_key_name
 
     tags = {
-      Name = "arpit-devops"
+      Name = var.vm-name
     }
 }
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "arpit-ami-id" {
+
+    type = string
+    description = "this is my sydney aws region ami id"
+    
+  
+}
+
+variable "vm-name" {
+
+      type = string 
+}
+
+variable "vm-size" {
+
+     type = string
+  
+}
+
+variable "ec2_key_name" {
+
+    type = string
+  
+}
