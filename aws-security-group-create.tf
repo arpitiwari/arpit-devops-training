29852cc541a64a14ea405af6c205a9fef3e26e82
Arpit Tiwari <arpi2art@gmail.com>
2025-11-06 06:45:02 +0000

    changing ingress cidr to 0.0.0.0/0 for sg

--- aws-security-group-create.tf
+++ aws-security-group-create.tf
@@ -19,15 +19,15 @@ resource "aws_security_group" "arpit-security-group" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.arpit-security-group.id
-  cidr_ipv4         = data.aws_vpc.arpit-vpc.cidr_block
-  from_port         = 443
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.arpit-security-group.id
-   cidr_ipv4         = data.aws_vpc.arpit-vpc.cidr_block
+   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
